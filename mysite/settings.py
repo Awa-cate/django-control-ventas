39a44ff964db0ae4c19abd15307762cf8798824e
Awa-cate <150825624+Awa-cate@users.noreply.github.com>
2025-01-28 10:40:23 -0500

    solved error

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': 
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         dj_database_url.config(
             default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
-    }
+    
 }
 
 
